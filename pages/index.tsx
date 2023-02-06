e00b64014b1d6c8a1adf5f0cbcc796254451fab7
Saksham Gupta <gupta.saksham98@gmail.com>
2023-02-06 09:39:45 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -118,12 +118,12 @@ const Home: NextPage = () => {
           <div className="block">
             <DropDown vibe={vibe} setVibe={(newVibe) => setVibe(newVibe)} />
             <textarea
-            value={jobd}
-            onChange={(e) => setjobd(e.target.value)}
+            value={bio}
+            onChange={(e) => setBio(e.target.value)}
             rows={4}
             className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-5"
             placeholder={
-              "e.g. Senior Developer Advocate @vercel. Tweeting about web development, AI, and React / Next.js. Writing nutlope.substack.com."
+              "Qualifications: Exposure to Data Scientist or Data Analyst roles (Previous Internship/Portfolio),building models, machine-learning, NLP, R, SQL, and Python"
             }
           />
           </div>
